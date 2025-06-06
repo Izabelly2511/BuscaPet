a3d30c80d0005a641d86125340f855aeeb9aa89d
Sesi3 <Sesi3@SP-1597639>
2025-06-06 11:15:58 -0300

    criacao da controller tag

---
+++ Controllers/TagsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuscaPet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+    }
+}
